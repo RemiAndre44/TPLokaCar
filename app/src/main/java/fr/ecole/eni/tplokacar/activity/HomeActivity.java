106e0a2f59254599da4ede98f3bb218cb143458c
maxfqs <maxfqs@gmail.com>
2018-04-27 09:43:23 +0200

    Create App class and activity package

--- app/src/main/java/fr/ecole/eni/tplokacar/activity/HomeActivity.java
+++ app/src/main/java/fr/ecole/eni/tplokacar/activity/HomeActivity.java
@@ -1,7 +1,9 @@
-package fr.ecole.eni.tplokacar;
+package fr.ecole.eni.tplokacar.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import fr.ecole.eni.tplokacar.R;
 
 public class HomeActivity extends AppCompatActivity {
 
