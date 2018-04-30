0e1e36ef78249415a7b5da074421bfe9e6d50ab1
RemiAndre44 <remi44andre@gmail.com>
2018-04-30 17:22:02 +0200

    Update

--- app/src/main/java/fr/ecole/eni/tplokacar/activity/DetailClientActivity.java
+++ app/src/main/java/fr/ecole/eni/tplokacar/activity/DetailClientActivity.java
@@ -1,7 +1,10 @@
 package fr.ecole.eni.tplokacar.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import fr.ecole.eni.tplokacar.R;
 
@@ -11,5 +14,18 @@ public class DetailClientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_client);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajoute les entrées de menu_test à l'ActionBar
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void onClickHome(MenuItem item) {
+        Intent intent = new Intent(DetailClientActivity.this, HomeActivity.class);
+
+        startActivity(intent);
     }
 }
