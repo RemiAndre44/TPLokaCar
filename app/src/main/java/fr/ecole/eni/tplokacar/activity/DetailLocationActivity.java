98e5f6d78ca90cb09cfc1b15e7f495851a66ef73
RemiAndre44 <remi44andre@gmail.com>
2018-05-02 17:21:14 +0200

    Update

--- app/src/main/java/fr/ecole/eni/tplokacar/activity/DetailLocationActivity.java
+++ app/src/main/java/fr/ecole/eni/tplokacar/activity/DetailLocationActivity.java
@@ -3,16 +3,54 @@ package fr.ecole.eni.tplokacar.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 import fr.ecole.eni.tplokacar.R;
+import fr.ecole.eni.tplokacar.database.entity.Client;
+import fr.ecole.eni.tplokacar.database.entity.Location;
+import fr.ecole.eni.tplokacar.database.entity.Vehicule;
 
 public class DetailLocationActivity extends ActivityWithMenu {
+    TextView nomClient;
+    TextView marqueVehicule;
+    TextView modeleVehicule;
+    TextView immatriculationVehicule;
+    TextView nbPlacesVehicule;
+    TextView dateDepartLocation;
+    TextView dureeLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_location);
+
+
+    }
+
+    public void chargementDonnee(){
+        Intent intent =getIntent();
+        Client client = new Client();
+        client= intent.getParcelableExtra("");
+        Vehicule vehicule = new Vehicule();
+        vehicule = intent.getParcelableExtra("");
+        Location location = new Location();
+        location = intent.getParcelableExtra("");
+
+        nomClient = findViewById(R.id.nomClientLocation);
+        nomClient.setText(client.getNom()+"#"+client.getId_client());
+        marqueVehicule = findViewById(R.id.marqueVehiculeLocaton);
+        marqueVehicule.setText(vehicule.getMarque());
+        modeleVehicule = findViewById(R.id.modeleVehiculeLocation);
+        modeleVehicule.setText(vehicule.getModele());
+        immatriculationVehicule = findViewById(R.id.imatVehiculeLocation);
+        immatriculationVehicule.setText(vehicule.getPlaque());
+        nbPlacesVehicule = findViewById(R.id.nbPlacesVehiculeLocation);
+        nbPlacesVehicule.setText(vehicule.getNbrePlaces());
+        dateDepartLocation = findViewById(R.id.dateDepartVehiculeLocation);
+        //dateDepartLocation.setText(Integer.toString(location.getDateDepart()));
+        dureeLocation = findViewById(R.id.dureeVehiculeLocation);
+        dureeLocation.setText(Integer.toString(location.getDuree()));
 
 
     }
